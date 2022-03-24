7d669eadd9270b522016a7dde368fbb8517f0781
obafemi <oludahunsiobafemi18@gmail.com>
2022-03-24 21:11:57 +0100

    implemented the use of filter for the validation of email input by the users and the use of Regex for the validation of the pizza title and ingredients inputs by the users.

--- add.php
+++ add.php
@@ -1,5 +1,37 @@
 <?php 
 
+    if(isset($_POST['submit'])){
+        // validation for an empty email
+        if(empty($_POST['email'])){
+            echo 'An email is required <br /> ';
+        } else {
+            $email = $_POST['email'];
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                echo 'Enter a valid E-maiil address. ';
+            }
+        };
+
+        // validation for an empty title
+        if(empty($_POST['title'])){
+            echo 'A title is required <br />';
+        } else {
+            $title = $_POST['title'];
+            if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+                echo 'Enter a title with just normal characters and space.';
+            }
+        };
+        
+        // validation for an empty ingredient
+        if(empty($_POST['ingredients'])){
+            echo 'An ingredient is required <br />';
+        } else {
+            $ingredients = $_POST['ingredients'];
+            if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+                echo  "Ingredients must be separated by commas's ";
+            }
+        }
+    };
+
 
 ?>
 
@@ -9,7 +41,7 @@
 
     <container class="container grey-text">
         <h4 class="center"> Add a Pizza </h4>
-        <form action="" class="white" method="" >
+        <form action="add.php" class="white" method="POST" >
             <label for=""> Your Email: </label>
             <input type="text" name="email" >
             <label for=""> Pizza Title: </label>
