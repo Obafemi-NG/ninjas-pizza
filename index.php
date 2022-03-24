12dbf72ba79a25bc8a2fb8eaf526c4011ba13e11
obafemi <oludahunsiobafemi18@gmail.com>
2022-03-24 17:38:43 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+    // $name = 'joshua';
+    // function my_func() {
+    //     global $name;
+    //     $name = 'Obafemi';
+    //     echo "my name is $name";
+    // };
+
+    // my_func();
+    // echo $name;
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title> My  PHP  </title>
+</head>
+<body>
+    
+</body>
+</html>
